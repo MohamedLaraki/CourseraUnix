e6df7e51c9ce3fd73fd7781f1e31431c4fe25659
Mohamed Laraki Hossini <m.larakihossini@gmail.com>
2025-01-25 02:15:20 +0100

    Initial commit with guessinggame.sh and Makefile

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,30 @@
-"" 
+#!/usr/bin/env bash
+
+# Function to count files in the current directory
+count_files() {
+  echo $(ls -1 | wc -l)
+}
+
+# Main loop to guess the number of files
+guess_number() {
+  correct=$(count_files)
+  guess=0
+
+  while [[ $guess -ne $correct ]]
+  do
+    echo "Guess the number of files in the current directory: "
+    read guess
+
+    if [[ $guess -lt $correct ]]
+    then
+      echo "Too low!"
+    elif [[ $guess -gt $correct ]]
+    then
+      echo "Too high!"
+    else
+      echo "Congratulations! You guessed correctly."
+    fi
+  done
+}
+
+guess_number
